76420d6d0740654ab4327ed745db754074a8275b
yjleeinkr <lucy.lee930216@gmail.com>
2023-05-14 23:44:40 +0900

    use generateStaticParams for SSG on some pages

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -8,3 +8,10 @@ export default function PantsPage({ params }: Props) {
     <h1>{params.slug} 제품 설명 페이지</h1>
   )
 }
+
+export function generateStaticParams() {
+  const products = ['pants', 'skirt'];
+  return products.map(product => ({
+    slug: product,
+  }))
+}
