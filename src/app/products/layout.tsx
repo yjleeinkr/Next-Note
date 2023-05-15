6a6050a8d6bd0e554a370ccaabdd988537337411
yjleeinkr <lucy.lee930216@gmail.com>
2023-05-16 01:12:16 +0900

    set static metadata

--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -1,9 +1,19 @@
+import { Metadata } from 'next'
 import Link from 'next/link'
 import styles from './layout.module.css'
 
 type ChildrenProps = {
   children : React.ReactNode
 }
+
+export const metadata: Metadata = {
+  title: "Cool Site | 전체 제품 확인",
+  description: "멋진  제품을 확인해 보세요",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
 export default function ProductLayout({ children } : ChildrenProps) {
   return (
     <>
