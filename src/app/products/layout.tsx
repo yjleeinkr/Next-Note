c2716c1ec93e70de578c53e7070b77c4b2b742b4
yjleeinkr <lucy.lee930216@gmail.com>
2023-05-16 00:43:24 +0900

    use Link on products page

--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -6,12 +6,12 @@ type ChildrenProps = {
 }
 export default function ProductLayout({ children } : ChildrenProps) {
   return (
-    <div>
+    <>
       <nav className={styles.nav}>
         <Link href="/products/woman">여성복</Link>
         <Link href="/products/man">남성복</Link>
       </nav>
       <section className={styles.productContainer}>{children}</section>
-    </div>
+    </>
   );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,19 @@
+import Link from "next/link"
+
+const products = ['shirt', 'pants', 'skirt', 'shoes']; // 임시 데이터
+
 export default function ProductsPage() {
+
   return (
-    <div>products page</div>
-  )
+    <>
+      <h1>제품 소개 페이지</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product}>
+            <Link href={`/products/${product}`}>{product}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
