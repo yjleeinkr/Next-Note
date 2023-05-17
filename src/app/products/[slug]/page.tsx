128bb4c632ffc69818aa8b857da12af1ac091b15
yjleeinkr <lucy.lee930216@gmail.com>
2023-05-17 14:32:55 +0900

    refac : separate fetching data logic from component

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { getProduct, getProducts } from "@/service/products";
 import { notFound } from "next/navigation";
 
 type Props = {
@@ -12,16 +13,20 @@ export function generateMetadata({ params }: Props) {
   }
 }
 
-export default function PantsPage({ params }: Props) {
-  if (params.slug === "nothing") {
+export default async function ProductPage({ params: { slug } }: Props) {
+  const product = await getProduct(slug);
+  if (!product) {
     notFound();
   }
-  return <h1>{params.slug} 제품 설명 페이지</h1>;
+  return (
+    <h1>{product.name} 제품 설명 페이지</h1>
+  );
 }
 
-export function generateStaticParams() {
-  const products = ["pants", "skirt"];
+export async function generateStaticParams() {
+  // 모든 제품의 페이지들을 미리 만들어 둘 수 있게 하자 (SSG)
+  const products = await getProducts();
   return products.map((product) => ({
-    slug: product,
+    slug: product.id,
   }));
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,16 +1,15 @@
+import { getProducts } from "@/service/products";
 import Link from "next/link"
 
-const products = ['shirt', 'pants', 'skirt', 'shoes']; // 임시 데이터
-
-export default function ProductsPage() {
-
+export default async function ProductsPage() {
+  const products = await getProducts();
   return (
     <>
       <h1>제품 소개 페이지</h1>
       <ul>
-        {products.map((product) => (
-          <li key={product}>
-            <Link href={`/products/${product}`}>{product}</Link>
+        {products.map(({id, name}, idx) => (
+          <li key={idx}>
+            <Link href={`/products/${id}`}>{name}</Link>
           </li>
         ))}
       </ul>
