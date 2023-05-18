09829e85adb847f5f837e0a95d8c20324166f04a
yjleeinkr <lucy.lee930216@gmail.com>
2023-05-19 00:48:02 +0900

    optimizing font via next/font and use variable fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Open_Sans, Nanum_Gothic } from "next/font/google";
 import Link from 'next/link'
 import './globals.css'
 import styles from './layout.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const sans = Open_Sans({ subsets: ['latin'] });
+const nanum = Nanum_Gothic({ weight: '700', subsets: ['latin']});
 
 export const metadata: Metadata = {
   title: 'Cool Site',
@@ -17,9 +18,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={sans.className}>
         <header className={styles.header}>
-          <h1>Demo Note</h1>
+          <h1 className={nanum.className}>Demo Note</h1>
           <nav className={styles.nav}>
             <Link href="/products">Products</Link>
             <Link href="/about">About</Link>
