8461a6f73d935922010aeb7f73780122bec5bc0b
yjleeinkr <lucy.lee930216@gmail.com>
2023-05-19 12:59:27 +0900

    use middleware and filter it to run on specific paths using config-matcher

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  console.log('미들웨어가 실행되고 있음')
+  if (request.nextUrl.pathname.startsWith('/products/1004')) {
+    console.log('미들웨어 - 경로를 리다이렉팅함')
+    console.log(new URL("/products", request.url));
+    return NextResponse.redirect(new URL('/products', request.url))
+  }
+}
+
+export const config = {
+  matcher: ['/products/:path+']
+}
