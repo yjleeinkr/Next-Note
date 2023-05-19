8fb83f349ea8550505800ef3ed0e3d0802b0b310
yjleeinkr <lucy.lee930216@gmail.com>
2023-05-19 14:17:11 +0900

    vercel test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   console.log('안녕 서버', os.hostname())
   return (
     <>
-      <h1>홈페이지</h1>
+      <h1>홈페이지 v2</h1>
       <Counter />
       <Image src="https://images.unsplash.com/photo-1441986300917-64674bd600d8" alt="shop" width={400} height={400}/>
     </>
